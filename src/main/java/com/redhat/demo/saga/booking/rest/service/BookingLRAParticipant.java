f02227f6128f302a2d2efbf55f45b5a311e72806
César Rodríguez Medina <cesar.rodriguez.medina@ibm.com>
2020-07-29 13:41:28 +0200

    all

--- src/main/java/com/redhat/demo/saga/booking/rest/service/BookingLRAParticipant.java
+++ src/main/java/com/redhat/demo/saga/booking/rest/service/BookingLRAParticipant.java
@@ -41,35 +41,35 @@ import org.eclipse.microprofile.lra.client.LRAClient;
 public class BookingLRAParticipant {
 
     private static final Logger LOG = Logger.getLogger(BookingLRAParticipant.class.getName());
-    
+
     private Client ticketClient;
 
     private Client insuranceClient;
-    
+
     private Client paymentClient;
 
     private WebTarget ticketTarget;
 
     private WebTarget insuranceTarget;
-    
+
     private WebTarget paymentTarget;
-    
+
     @PostConstruct
     private void init() {
         ticketClient = ClientBuilder.newClient();
         insuranceClient = ClientBuilder.newClient();
         paymentClient = ClientBuilder.newClient();
-        ticketTarget = 
-                ticketClient.target("http://" + System.getenv("TICKET_LRA_THORNTAIL_SERVICE_HOST") 
-                        + ":" + System.getenv("TICKET_LRA_THORNTAIL_SERVICE_PORT" ) 
+        ticketTarget =
+                ticketClient.target("http://" + System.getenv("TICKET_SERVICE_HOST")
+                        + ":" + System.getenv("TICKET_SERVICE_PORT" )
                         + "/tickets");
-        insuranceTarget = 
-                insuranceClient.target("http://" + System.getenv("INSURANCE_LRA_THORNTAIL_SERVICE_HOST") 
-                        + ":" + System.getenv("INSURANCE_LRA_THORNTAIL_SERVICE_PORT" ) 
+        insuranceTarget =
+                insuranceClient.target("http://" + System.getenv("INSURANCE_LRA_THORNTAIL_SERVICE_HOST")
+                        + ":" + System.getenv("INSURANCE_LRA_THORNTAIL_SERVICE_PORT" )
                         + "/insurances");
-        paymentTarget = 
-                paymentClient.target("http://" + System.getenv("PAYMENT_LRA_THORNTAIL_SERVICE_HOST") 
-                        + ":" + System.getenv("PAYMENT_LRA_THORNTAIL_SERVICE_PORT" ) 
+        paymentTarget =
+                paymentClient.target("http://" + System.getenv("PAYMENT_LRA_THORNTAIL_SERVICE_HOST")
+                        + ":" + System.getenv("PAYMENT_LRA_THORNTAIL_SERVICE_PORT" )
                         + "/payments");
     }
 
@@ -104,7 +104,7 @@ public class BookingLRAParticipant {
          * method finishes at which point the completeWork method below will be
          * invoked.
          */
-        
+
         String lraId = lraIdUrl.substring(lraIdUrl.lastIndexOf('/') + 1);
         LOG.log(Level.INFO, "Value of lraIdUrl in book method {0}", lraId);
         // #### TICKET ####
@@ -113,30 +113,30 @@ public class BookingLRAParticipant {
         Response response
                 = ticketTarget.request().post(Entity.entity(
                         ticket, MediaType.APPLICATION_JSON_TYPE));
-        
+
         LOG.log(Level.INFO, "Ticket created with response code {0}", response.getStatus());
         Ticket readEntity = response.readEntity(Ticket.class);
         LOG.log(Level.INFO, "Value of readEntity in book method {0}", readEntity);
-        
+
         // #### INSURANCE ####
-        Insurance insurance = new Insurance(readEntity.getOrderId(), InsuranceState.INSURANCE_BOOKED_PENDING, 
+        Insurance insurance = new Insurance(readEntity.getOrderId(), InsuranceState.INSURANCE_BOOKED_PENDING,
                 readEntity.getAccountId(), "PROTECT_ALL", readEntity.getTotalCost() / 2, readEntity.getId(), lraId);
         Response responseInsurance
                 = insuranceTarget.request().post(Entity.entity(
                         insurance, MediaType.APPLICATION_JSON_TYPE));
-        
+
         // #### PAYMENT ####
         Double orderCost = readEntity.getTotalCost() + insurance.getTotalCost();
-        Payment payment = new Payment(readEntity.getOrderId(), orderCost, PaymentState.PAYMENT_INPROGRESS, 
+        Payment payment = new Payment(readEntity.getOrderId(), orderCost, PaymentState.PAYMENT_INPROGRESS,
                 readEntity.getAccountId(), lraId);
         Response responsePayment
                 = paymentTarget.request().post(Entity.entity(
                         payment, MediaType.APPLICATION_JSON_TYPE));
-        
+
         if (orderCost > 100) {
             throw new WebApplicationException("Too Expensive!!!!!! NOT AVAILABLE", Response.Status.NOT_ACCEPTABLE);
         }
-        
+
         return Response.ok(readEntity.getOrderId()).status(201).build();
     }
 
@@ -153,35 +153,35 @@ public class BookingLRAParticipant {
          */
         String lraId = lraIdUrl.substring(lraIdUrl.lastIndexOf('/') + 1);
         LOG.log(Level.INFO, "Value of lraId in completeBook method {0}", lraId);
-        
+
         // #### TICKET ####
         Ticket ticket
                 = ticketTarget.path("lraId/"
                         + lraId).request().get(Ticket.class);
         ticket.setState(TicketState.TICKET_BOOKED);
-        
+
         ticketTarget.path(ticket.getId().toString()).request().put(
                 Entity.entity(ticket, MediaType.APPLICATION_JSON_TYPE));
-        
+
         // #### INSURANCE ####
         Insurance insurance
                 = insuranceTarget.path("lraId/"
                         + lraId).request().get(Insurance.class);
         insurance.setState(InsuranceState.INSURANCE_BOOKED);
-        
+
         insuranceTarget.path(insurance.getId().toString()).request().put(
                 Entity.entity(insurance, MediaType.APPLICATION_JSON_TYPE));
-        
+
         // #### PAYMENT ####
         Payment payment
                 = paymentTarget.path("lraId/"
                         + lraId).request().get(Payment.class);
         payment.setState(PaymentState.PAYMENT_ACCEPTED);
-        
+
         paymentTarget.path(payment.getId().toString()).request().put(
                 Entity.entity(payment, MediaType.APPLICATION_JSON_TYPE));
-        
-        
+
+
         return Response.ok("Completed").build();
     }
 
@@ -199,36 +199,36 @@ public class BookingLRAParticipant {
          */
         String lraId = lraIdUrl.substring(lraIdUrl.lastIndexOf('/') + 1);
         LOG.log(Level.INFO, "Value of lraId in compensateWork method {0}", lraId);
-        
+
         // #### TICKET ####
         Ticket ticket
                 = ticketTarget.path("lraId/"
                         + lraId).request().get(Ticket.class);
         ticket.setState(TicketState.TICKET_AVAILABLE);
-        
+
         LOG.log(Level.INFO, "Value of ticket id in compensateWork method {0}", ticket.getId());
-        
+
         ticketTarget.path(ticket.getId().toString()).request().put(
                 Entity.entity(ticket, MediaType.APPLICATION_JSON_TYPE));
-        
+
         // #### INSURANCE ####
         Insurance insurance
                 = insuranceTarget.path("lraId/"
                         + lraId).request().get(Insurance.class);
         insurance.setState(InsuranceState.INSURANCE_PAYMENT_REFUSED);
-        
+
         insuranceTarget.path(insurance.getId().toString()).request().put(
                 Entity.entity(insurance, MediaType.APPLICATION_JSON_TYPE));
-        
+
         // #### PAYMENT ####
         Payment payment
                 = paymentTarget.path("lraId/"
                         + lraId).request().get(Payment.class);
         payment.setState(PaymentState.PAYMENT_REFUSED);
-        
+
         paymentTarget.path(payment.getId().toString()).request().put(
                 Entity.entity(payment, MediaType.APPLICATION_JSON_TYPE));
-        
+
         return Response.ok("COMPENSATED").build();
     }
 
